5f4d8ad16188e2af5ccc56b93e59c69e127ac64b
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-07-08 13:11:24 +0500

    refactor: updated to adding series title

--- class-components/src/services/DataFetch.ts
+++ class-components/src/services/DataFetch.ts
@@ -19,12 +19,32 @@ export interface FetchEpisodesResponse {
   };
 }
 
+export interface ApiEpisode {
+  uid: string;
+  title: string;
+  seasonNumber: number;
+  episodeNumber: number;
+  series: {
+    uid: string;
+    title: string;
+  };
+}
+
+export interface ApiFetchEpisodesResponse {
+  episodes: ApiEpisode[];
+  page: {
+    pageNumber: number;
+    pageSize: number;
+    numberOfElements: number;
+    totalElements: number;
+    totalPages: number;
+    firstPage: boolean;
+    lastPage: boolean;
+  };
+}
+
 export class FetchEpisodes {
-  getEpisodes = async (
-    searchItem: string,
-    pageNumber: number,
-    pageSize: number,
-  ): Promise<FetchEpisodesResponse> => {
+  getEpisodes = async (searchItem: string, pageNumber: number, pageSize: number): Promise<FetchEpisodesResponse> => {
     const url = `https://stapi.co/api/v1/rest/episode/search?title=${encodeURIComponent(searchItem)}&pageNumber=${pageNumber}&pageSize=${pageSize}`;
     const response = await fetch(url);
 
@@ -32,16 +52,16 @@ export class FetchEpisodes {
       throw new Error(`Not fetched ${url}, status: ${response.status}`);
     }
 
-    const data: FetchEpisodesResponse = await response.json();
+    const data: ApiFetchEpisodesResponse = await response.json();
     return {
-      episodes: data.episodes.map((episode) => ({
+      episodes: data.episodes.map((episode: ApiEpisode) => ({
         uid: episode.uid,
         title: episode.title,
         seasonNumber: episode.seasonNumber,
         episodeNumber: episode.episodeNumber,
-        seriesTitle: episode.seriesTitle || '',
+        seriesTitle: episode.series.title,
       })),
       page: data.page,
     };
-  };
+  }
 }
