ccf0cf29ca93c2012b5386911e42ce4121495414
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-07-08 11:41:46 +0500

    feat: added spinner and configured svg imports

---
+++ class-components/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import { Component, ReactNode } from "react";
+import  SpinnerSVG  from "../../assets/spinner.svg"
+
+export class Spinner extends Component {
+  render(): ReactNode {
+    return (
+      <SpinnerSVG />
+    )
+  }
+}
