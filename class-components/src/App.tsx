20344ae4cff904a848f6961a09a25d746a0fe848
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-07-14 18:32:58 +0500

    fix: added ErrorBoundary

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SearchComponent } from './components/SearchComponent/SearchComponent';
-import DetailComponent from './components/Detail/Detail';
 import './App.css';
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div id="app">
-        <Routes>
-          <Route path="/" element={<SearchComponent />} />
-          <Route path="/detail/:uid" element={<DetailComponent />} />
-        </Routes>
-      </div>
+      <ErrorBoundary>
+        <div id="app">
+          <Routes>
+            <Route path="/" element={<SearchComponent />} />
+          </Routes>
+        </div>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
