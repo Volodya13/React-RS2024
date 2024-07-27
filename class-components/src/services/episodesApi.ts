c30d00247324c5a42180102d9bdc3e6de19c61b3
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2024-07-27 14:55:33 +0500

    refactor: updated episodeAPI, added POST query.

--- class-components/src/services/episodesApi.ts
+++ class-components/src/services/episodesApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IEpisode } from '../interfaces/IEpisode';
-import { IFetchEpisodesResponse } from '../interfaces/IFetchEpisodesResponse';
+import { IData } from '../interfaces/IData.ts';
+import { IEpisode } from '../interfaces/IEpisode.ts';
 
 const BASE_URL = 'https://stapi.co/api/v1/rest';
 
@@ -8,26 +8,35 @@ export const episodesAPI = createApi({
   reducerPath: 'episodesAPI',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
-    getAllEpisodes: builder.query<IFetchEpisodesResponse, { pageNumber: number; pageSize: number }>(
-      {
-        query: ({ pageNumber, pageSize = 9 }) => ({
-          url: `/episode/search`,
-          params: {
-            pageNumber,
-            pageSize,
-          },
-        }),
-      },
-    ),
+    getAllEpisodes: builder.query<IData, number>({
+      query: (page = 0) => ({
+        url: `/episode/search`,
+        params: {
+          pageNumber: page,
+          pageSize: 7,
+        },
+      }),
+    }),
     getEpisodeById: builder.query<IEpisode, string>({
       query: (id) => ({
-        url: `/episode/search`,
+        url: `/episode`,
         params: {
           uid: id,
         },
       }),
     }),
+    searchEpisodes: builder.query<IData, { title: string }>({
+      query: (searchParams) => ({
+        url: '/episode/search',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(searchParams as Record<string, string>),
+      }),
+    }),
   }),
 });
 
-export const { useGetAllEpisodesQuery, useGetEpisodeByIdQuery } = episodesAPI;
+export const { useGetAllEpisodesQuery, useGetEpisodeByIdQuery, useLazySearchEpisodesQuery } =
+  episodesAPI;
